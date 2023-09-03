ef93162115ed3653ed24f3c904888a935efbfc1f
Flavio Jr <fjs73santosjunior@gmail.com>
2023-09-03 16:51:56 -0300

    atualizando projeto

--- apps/precificacao/urls.py
+++ apps/precificacao/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.precificacao.views import index
+from apps.precificacao.views import index, produto_individual, item_indivdual
 
 urlpatterns = [
     path('', index, name='index'),
+    path('produto_individual', produto_individual, name='produto_individual'),
+    path('item_indivdual', item_indivdual, name='item_indivdual'),
 ]
--- apps/precificacao/views.py
+++ apps/precificacao/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def index( request ):
     url = "precifica/produtos/dashboard_produtos.html"
     return render(request, url)
+
+def produto_individual( request ):
+    return render(request, "precifica/produtos/produto-individual.html")
+
+def item_indivdual( request ):
+    return render( request, "precifica/itens/item_individual_comprado.html")
